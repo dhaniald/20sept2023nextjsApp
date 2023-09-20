a110174eaaa0583dec6db673c59f42e46afdc0ba
Gaurav <gtiwari95@gmail.com>
2023-09-21 02:11:17 +0530

    chanegd code syntax of navigation

--- pages/index.js
+++ pages/index.js
@@ -1,83 +1,84 @@
 
-//1 import area
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+  //1 import area
+  import Head from 'next/head'
+  import Image from 'next/image'
+  import styles from '../styles/Home.module.css'
 
-//import something from 'some library/location'
+  //import something from 'some library/location'
 
-//import DefaultImport from '../somelocations'
-import TITLE from './helper/constant';
+  //import DefaultImport from '../somelocations'
+  import TITLE from './helper/constant';
 
-//import { NamedExport } from 'some library/location'
-import { CONTENT } from './helper/constant2';
-import Link from 'next/link';
+  //import { NamedExport } from 'some library/location'
+  import { CONTENT } from './helper/constant2';
+  import Link from 'next/link';
 
-//2. defenation area
-function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>{TITLE}</title>
-        <meta name="description" content={CONTENT} />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+  //2. defenation area
+  function Home() {
+    return (
+      <div className={styles.container}>
+        <Head>
+          <title>{TITLE}</title>
+          <meta name="description" content={CONTENT} />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to my Website
-        </h1>
+        <main className={styles.main}>
+          <h1 className={styles.title}>
+            Welcome to my Website
+          </h1>
 
-        <h1 className={styles.title}>
-          <Link href="/about"> About Us</Link>
-        </h1>
+          <h1 className={styles.title}>
+            <Link href="/about"> About Us</Link>
+          </h1>
 
-        <h1 className={styles.title}>
-          <Link href="/contact">Contact Us</Link>
-        </h1>
+          <h1 className={styles.title}>
+            <Link href="/contact">Contact Us</Link>
+          </h1>
+          <div className={styles.grid}>
+          <p>
+          <h2 className={styles.card}>Lucknow &rarr;</h2>
+           <Link href="/Lucknow/hotel">Click Here to find.</Link></p>   
+          
+          <p>
+          <h2 className={styles.card}>Prayagraj &rarr;</h2>
+          <Link href="/Prayagraj/Hotel"> Click Here to find.</Link>
+          </p>
 
-        <div className={styles.grid}>
-          <a href="/Lucknow/hotel" className={styles.card}>
-            <h2>Lucknow &rarr;</h2>
-            <p>Click Here to find hotels in Lucknow.</p>
-          </a>
+          <p>
+          <h2 className={styles.card}>Bhopal &rarr;</h2>
+          <Link href="/Bhopal/Hotel">Click Here to find.</Link></p>
 
-          <a href="/Bhopal/Hotel" className={styles.card}>
-            <h2>Bhopal &rarr;</h2>
-            <p>Click Here to findHotels in Bhopal!</p>
-          </a>
 
-          <a href="/Prayagraj/Hotel" className={styles.card}>
-            <h2>Prayagraj &rarr;</h2>
-            <p>Click Here to findHotels in Bhopal!</p>
-          </a>
 
+          
+
+            <a
+              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              className={styles.card}
+            >
+              <h2>Deploy &rarr;</h2>
+              <p>
+                Instantly deploy your Next.js site to a public URL with Vercel.
+              </p>
+            </a>
+          </div>
+        </main>
+
+        <footer className={styles.footer}>
           <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            Powered by{' '}
+            <span className={styles.logo}>
+              <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            </span>
           </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    </div>
-  )
-}
-//3. export area
-export default Home;
+        </footer>
+      </div>
+    )
+  }
+  //3. export area
+  export default Home;
